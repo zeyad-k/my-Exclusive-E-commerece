0f6f1ec96f803b52e56ffd57f54c88aacda350c1
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-05-07 12:00:49 +0300

    style: Update Header and HeaderNavigation components
    Refactor the Header and HeaderNavigation components to improve the styling and responsiveness. Add margin-top and padding to the header for better spacing. Update the navigation links to have transition effects on hover and active states. Also, use the BEM naming convention for class names and improve the font styles and colors to match the design specifications.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="mx-auto flex max-w-[1200px] justify-between">
+    <header className="mx-auto mt-10 flex max-w-[1200px] justify-between py-2">
       <Logo />
       <HeaderNavigation />
       <HeaderIcons />
--- src/components/HeaderNavigation.jsx
+++ src/components/HeaderNavigation.jsx
@@ -3,18 +3,38 @@ import { Link } from "react-router-dom";
 const HeaderNavigation = () => {
   return (
     <nav>
-      <ul>
+      <ul className="flex w-[367px] justify-between gap-2 text-center font-poppins text-base font-normal leading-6 text-black">
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            className="transition duration-200 ease-in-out hover:underline  active:underline"
+            to="/"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/">Contact</Link>
+          <Link
+            className="transition duration-200 ease-in-out hover:underline  active:underline"
+            to="/"
+          >
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            className="transition duration-200 ease-in-out hover:underline  active:underline"
+            to="/about"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/about">Sign Up</Link>
+          <Link
+            className="transition duration-200 ease-in-out hover:underline  active:underline"
+            to="/about"
+          >
+            Sign Up
+          </Link>
         </li>
       </ul>
     </nav>
