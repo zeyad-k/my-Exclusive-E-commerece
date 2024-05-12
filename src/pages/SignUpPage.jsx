81a731a5f32db715acfc2f9b195e474470cd04db
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-05-12 14:45:13 +0300

    refactor: Update SignUpPage component layout and styling
    Refactor the SignUpPage component to improve its layout and styling. Update the class names to follow the BEM naming convention and add a container class for consistent spacing and alignment. This enhances the visual consistency and maintainability of the sign-up page.

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,14 +2,14 @@ import SignUpForm from "../components/customerAuth/SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <div className="relative overflow-y-hidden">
+    <div className="relative overflow-y-hidden md:lg:my-16">
       <img
         src="/images/sideimg.png"
         className="absolute left-0 hidden max-h-full lg:block"
         alt="side image"
       />
       <section className="container ">
-        <div className="  items-center gap-40 py-16 lg:flex">
+        <div className="  items-center md:lg:flex md:lg:gap-40 md:lg:py-40">
           <SignUpForm />
         </div>
       </section>
