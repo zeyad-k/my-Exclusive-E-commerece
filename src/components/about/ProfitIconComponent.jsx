e87d96d31bf9d1293bb50c27ba69911f068a3592
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-05-18 01:22:59 +0300

    chore: Update ProfitIconComponent styling for better user experience

--- src/components/about/ProfitIconComponent.jsx
+++ src/components/about/ProfitIconComponent.jsx
@@ -7,7 +7,7 @@ const ProfitIconComponent = (props) => {
 
   return (
     <div
-      className="about-metric-overview-component flex h-64 w-64 flex-col items-center justify-center gap-2 rounded-md border border-black  border-opacity-30 px-12 py-8 hover:border-none hover:bg-main-active-color hover:text-white hover:shadow-lg"
+      className="about-metric-overview-component flex  h-60  w-72 flex-col items-center justify-center gap-2 rounded-md border border-black  border-opacity-30 px-12 py-8 hover:border-none hover:bg-main-active-color hover:text-white hover:shadow-lg"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -15,7 +15,7 @@ const ProfitIconComponent = (props) => {
       <h3 className="font-inter text-3xl font-extrabold leading-snug tracking-wide">
         {serviceNumber}K
       </h3>
-      <p className="text-center font-poppins text-base font-normal leading-6">
+      <p className="text-center font-poppins text-base font-normal  ">
         {serviceStatement}
       </p>
     </div>
