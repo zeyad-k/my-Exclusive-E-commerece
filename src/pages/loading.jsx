684c42d0310c79dadb044b7afd27379ae0eee58f
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-05-27 15:03:07 +0300

    refactor: Update stroke color in Loading component
    Update the stroke color in the Loading component from "#B563A1" to "#DB4444". This change improves the visual appearance of the component and ensures consistency with other UI elements.

--- src/pages/loading.jsx
+++ src/pages/loading.jsx
@@ -19,7 +19,7 @@ const Loading = () => {
           stroke-width="4"
           fill="none"
           id="outline"
-          stroke="#B563A1"
+          stroke="#DB4444"
         ></path>
         <path
           d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"
@@ -27,7 +27,7 @@ const Loading = () => {
           stroke-linejoin="round"
           stroke-linecap="round"
           stroke-width="4"
-          stroke="#B563A1"
+          stroke="#DB4444"
           fill="none"
           opacity="0.05"
           id="outline-bg"
